f36de9e618254c13847fdb8373a348ff25a8f61b
Gaheris2001 <bobby.strike@hotmail.fr>
2017-01-05 19:38:24 +0100

    Add files via upload
    Page html avec son canva et son bouton reset messieurs

---
+++ test2.js
@@ -0,0 +1,137 @@
+//var clickX = new Array();
+//var clickY = new Array();
+//var clickDrag = new Array();
+//var paint;
+var canvas;
+var context;
+var canvasWidth = 490;
+var canvasHeight = 220;
+var padding = 25;
+var lineWidth = 8;
+var colorPurple = "#cb3594";
+var colorGreen = "#659b41";
+var colorYellow = "#ffcf33";
+var colorBrown = "#986928";
+var outlineImage = new Image();
+var crayonImage = new Image();
+var markerImage = new Image();
+var eraserImage = new Image();
+var crayonBackgroundImage = new Image();
+var markerBackgroundImage = new Image();
+var eraserBackgroundImage = new Image();
+var crayonTextureImage = new Image();
+var clickX = new Array();
+var clickY = new Array();
+var clickColor = new Array();
+var clickTool = new Array();
+var clickSize = new Array();
+var clickDrag = new Array();
+var paint = false;
+var curColor = colorPurple;
+var curTool = "crayon";
+var curSize = "normal";
+var mediumStartX = 18;
+var mediumStartY = 19;
+var mediumImageWidth = 93;
+var mediumImageHeight = 46;
+var drawingAreaX = 111;
+var drawingAreaY = 11;
+var drawingAreaWidth = 267;
+var drawingAreaHeight = 200;
+var toolHotspotStartY = 23;
+var toolHotspotHeight = 38;
+var sizeHotspotStartY = 157;
+var sizeHotspotHeight = 36;
+var sizeHotspotWidthObject = new Object();
+sizeHotspotWidthObject.huge = 39;
+sizeHotspotWidthObject.large = 25;
+sizeHotspotWidthObject.normal = 18;
+sizeHotspotWidthObject.small = 16;
+var totalLoadResources = 8;
+var curLoadResNum = 0;
+
+
+function resourceLoaded()
+{
+	if(++curLoadResNum >= totalLoadResources){
+		redraw();
+	}
+}
+
+
+function prepareCanvas()
+{
+var canvasDiv = document.getElementById('canvasDiv');
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', canvasWidth);
+canvas.setAttribute('height', canvasHeight);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
+if(typeof G_vmlCanvasManager != 'undefined') {
+	canvas = G_vmlCanvasManager.initElement(canvas);
+}
+ context = canvas.getContext('2d');
+
+
+document.querySelector('#canvas').addEventListener('mousedown', function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+  
+  paint = true;
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  redraw();
+});
+
+document.querySelector('#canvas').addEventListener('mousemove', function(e){
+  if(paint){
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+});
+
+document.querySelector('#canvas').addEventListener('mouseup', function(e){
+  paint = false;
+});
+
+document.querySelector('#canvas').addEventListener('mouseleave', function(e){
+  paint = false;
+});
+}
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+  clickColor.push(curColor);
+}
+
+function redraw(){
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+  /* context.strokeStyle = "#df4b26"; */
+  context.lineJoin = "round";
+   context.lineWidth = 5; 
+			
+  for(var i=0; i < clickX.length; i++) 
+  {		
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+	 context.strokeStyle = clickColor[i];
+     context.stroke();
+	 //context.lineWidth = radius ;
+  }
+	
+}
+
+ function clearCanvas()
+{
+	context.clearRect(0, 0, canvasWidth, canvasHeight);
+	
+}
